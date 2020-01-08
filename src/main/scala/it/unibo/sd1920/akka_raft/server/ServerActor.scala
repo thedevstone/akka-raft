014025e8ecd455265b1965a68eff237743cc3b21
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2020-01-08 13:21:18 +0100

    Refactoring to trait

--- src/main/scala/it/unibo/sd1920/akka_raft/server/ServerActor.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/ServerActor.scala
@@ -1,47 +1,17 @@
 package it.unibo.sd1920.akka_raft.server
 
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props, Timers}
-import akka.cluster.{Cluster, Member}
-import akka.cluster.ClusterEvent.{MemberDowned, MemberUp}
+import akka.cluster.Cluster
 import com.typesafe.config.ConfigFactory
-import it.unibo.sd1920.akka_raft.client.ClientActor
-import it.unibo.sd1920.akka_raft.server.ServerActor.{ClientIdentity, IdentifyServer, ServerIdentity}
+import it.unibo.sd1920.akka_raft.utils.NetworkConstants
 import it.unibo.sd1920.akka_raft.utils.NodeRole.NodeRole
-import it.unibo.sd1920.akka_raft.utils.{NetworkConstants, NodeRole, RandomTime}
 
-import scala.concurrent.duration._
-
-class ServerActor extends Actor with ActorLogging with Timers {
-  private val cluster = Cluster(context.system)
-  private var servers: Map[String, ActorRef] = Map()
-  private var clients: Map[String, ActorRef] = Map()
-
-  override def preStart(): Unit = {
-    super.preStart()
-    cluster.subscribe(self, classOf[MemberUp], classOf[MemberDowned])
-    cluster.registerOnMemberUp({
-      timers.startTimerWithFixedDelay("","",  RandomTime.randomBetween(150,300) hours)
-    })
-  }
+private class ServerActor extends Actor with ServerActorDiscovery with ActorLogging with Timers {
+  protected[this] val cluster: Cluster = Cluster(context.system)
+  protected[this] var servers: Map[String, ActorRef] = Map()
+  protected[this] var clients: Map[String, ActorRef] = Map()
 
   override def receive: Receive = clusterBehaviour
-
-  private def clusterBehaviour: Receive = {
-    case MemberUp(member) => this.manageNewMember(member)
-    case MemberDowned(member) =>
-    case IdentifyServer(NodeRole.SERVER) => sender() ! ServerActor.ServerIdentity(self.path.name)
-    case IdentifyServer(NodeRole.CLIENT) => sender() ! ClientActor.ServerIdentity(self.path.name)
-    case ServerIdentity(name: String) => this.servers = this.servers + (name -> sender())
-    case ClientIdentity(name: String) => this.clients = this.clients + (name -> sender())
-  }
-
-  private def manageNewMember(member: Member): Unit = member match {
-    case m if member.roles.contains("server") =>
-      context.system.actorSelection(s"${m.address}/user/**") ! ServerActor.IdentifyServer(NodeRole.SERVER)
-    case m if member.roles.contains("client") =>
-      context.system.actorSelection(s"${m.address}/user/**") ! ClientActor.IdentifyClient(NodeRole.SERVER)
-    case _ =>
-  }
 }
 
 object ServerActor {
