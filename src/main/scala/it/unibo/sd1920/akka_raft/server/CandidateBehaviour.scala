aac5c39bdd9ebe1ee0e50327fcdc5083905a860e
Gyordan Caminati <gyordan.caminati@studio.unibo.it>
2020-01-11 17:46:53 +0100

    aggiunti comportamenti server

---
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/CandidateBehaviour.scala
@@ -0,0 +1,15 @@
+package it.unibo.sd1920.akka_raft.server
+
+import akka.cluster.ClusterEvent.{MemberDowned, MemberUp}
+import akka.cluster.Member
+import it.unibo.sd1920.akka_raft.client.ClientActor
+import it.unibo.sd1920.akka_raft.server.ServerActor.{ClientIdentity, IdentifyServer, ServerIdentity}
+import it.unibo.sd1920.akka_raft.utils.NodeRole
+
+private trait CandidateBehaviour {
+  this: ServerActor =>
+
+  protected def candidateBehaviour: Receive = {
+    case _ =>
+  }
+}
---
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/LeaderBehaviour.scala
@@ -0,0 +1,24 @@
+package it.unibo.sd1920.akka_raft.server
+
+import akka.cluster.ClusterEvent.{MemberDowned, MemberUp}
+import akka.cluster.Member
+import it.unibo.sd1920.akka_raft.client.ClientActor
+import it.unibo.sd1920.akka_raft.server.ServerActor.{ClientIdentity, IdentifyServer, ServerIdentity}
+import it.unibo.sd1920.akka_raft.utils.NodeRole
+
+private trait LeaderBehaviour {
+  this: ServerActor =>
+
+  private var followersStatusMap: Map[Int,FollowerStatus] = Map()
+
+
+  protected def leaderBehaviour: Receive = {
+    case _ =>
+
+  }
+}
+
+private class FollowerStatus(nextIndexToSend: Int, lastMatchIndex: Int){
+  assert(nextIndexToSend >= 0)
+  assert(lastMatchIndex >= 0)
+}
