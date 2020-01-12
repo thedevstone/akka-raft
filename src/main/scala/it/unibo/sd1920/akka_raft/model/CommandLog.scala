3d891d2739ef9638baabf7720671bcf04899741b
Sara Kiade <sara.kiade@studio.unibo.it>
2020-01-12 13:07:44 +0100

    lavorando sul follower behaviour

--- src/main/scala/it/unibo/sd1920/akka_raft/model/CommandLog.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/model/CommandLog.scala
@@ -16,7 +16,9 @@ class CommandLog[Command](private var entries: List[Entry[Command]]
   def nextIndex: Int = size
 
   def lastTerm: Int = this.entries.lastOption.map(t => t.term).getOrElse(0)
-  def lastIndex: Int = this.entries.lastOption.map(t => t.index).getOrElse(0)
+  def lastIndex: Int = this.entries.lastOption.map(t => t.index).getOrElse(-1)
+
+  def contains(entry: Entry[Command]): Boolean = entries.contains(entry)
 
   def getPreviousEntry(entry: Entry[Command]): Option[Entry[Command]] = entry.index match {
     case 0 => None
@@ -34,6 +36,15 @@ class CommandLog[Command](private var entries: List[Entry[Command]]
   def getCommitIndex: Int = this.commitIndex
   def append(entry: Entry[Command]): Unit = entries = entries :+ entry
   def remove(index: Int): Unit = entries = entries.slice(0, index)
+
+  def putElementAtIndex(entry: Entry[Command]): Boolean ={
+    if(entry.index > size && entry.index < 0) return false
+
+    if(entries.nonEmpty && entry.index < size ) this.remove(entry.index)
+
+    this.append(entry)
+    true
+  }
 }
 
 object CommandLog {
--- src/main/scala/it/unibo/sd1920/akka_raft/server/FollowerBehaviour.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/FollowerBehaviour.scala
@@ -8,7 +8,7 @@ import it.unibo.sd1920.akka_raft.client.ClientActor
 import it.unibo.sd1920.akka_raft.model.BankStateMachine.BankCommand
 import it.unibo.sd1920.akka_raft.model.Entry
 import it.unibo.sd1920.akka_raft.server.ServerActor.{ClientIdentity, ClientRequest, IdentifyServer, SchedulerTick, ServerIdentity}
-import it.unibo.sd1920.akka_raft.raft.{AppendEntries, AppendEntriesResult, RaftMessage, Redirect}
+import it.unibo.sd1920.akka_raft.raft.{AckAppendEntries, AppendEntries, AppendEntriesResult, RaftMessage, Redirect}
 
 
 private trait FollowerBehaviour {
@@ -17,8 +17,17 @@ private trait FollowerBehaviour {
   protected def followerBehaviour: Receive =  clusterBehaviour orElse  {
     case SchedulerTick => context.become(candidateBehaviour); startTimer()
     case ClientRequest(_,_) => sender() ! Redirect(Some(leaderRef))
-    //case AppendEntries(_,_,entry,_) if entry.isEmpty => startTimer() //il timer si resetta così? ok ma si deve sempre resettare non solo per hbeat
-    case appEntry: AppendEntries => handleNewAppend(appEntry, sender())
+
+    case AppendEntries(_,_,entry,_) if entry.isEmpty => startTimer(); sender() ! AckAppendEntries
+    case AppendEntries(leaderTerm, _, _, _) if leaderTerm < currentTerm => startTimer(); sender() ! AppendEntriesResult(false);
+
+    case AppendEntries(_, previousEntry, entry, _) if previousEntry.isEmpty => startTimer(); serverLog.putElementAtIndex(entry.get)
+    case AppendEntries(_, previousEntry, _, _) if  !serverLog.contains(previousEntry.get) => startTimer(); sender() ! AppendEntriesResult(false)
+/*
+    case AppendEntries(leaderTerm, previousEntry, entry, leaderLastCommit) if serverLog.contains(previousEntry.get)
+
+
+    case appEntry: AppendEntries => handleNewAppend(appEntry, sender())*/
   }
 
   private def handleNewAppend(appEntry: AppendEntries, leaderAddress: ActorRef): Unit ={
