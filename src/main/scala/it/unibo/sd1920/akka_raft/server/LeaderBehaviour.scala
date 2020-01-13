30b2790b0848a503ca61508c8613b6ab3d4f7125
Sara Kiade <sara.kiade@studio.unibo.it>
2020-01-13 17:44:27 +0100

    iniziato comportamento leader. Ancora a metà

--- src/main/scala/it/unibo/sd1920/akka_raft/server/LeaderBehaviour.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/LeaderBehaviour.scala
@@ -5,8 +5,10 @@ import akka.cluster.ClusterEvent.{MemberDowned, MemberUp}
 import akka.cluster.Member
 import akka.routing.ActorRefRoutee
 import it.unibo.sd1920.akka_raft.client.ClientActor
-import it.unibo.sd1920.akka_raft.raft.AppendEntriesResult
-import it.unibo.sd1920.akka_raft.server.ServerActor.{ClientIdentity, IdentifyServer, ServerIdentity}
+import it.unibo.sd1920.akka_raft.model.BankStateMachine.{ApplyCommand, BankCommand, CommandResult}
+import it.unibo.sd1920.akka_raft.model.Entry
+import it.unibo.sd1920.akka_raft.raft.{AckAppendEntries, AppendEntries, AppendEntriesResult, RequestVote}
+import it.unibo.sd1920.akka_raft.server.ServerActor.{ClientIdentity, ClientRequest, IdentifyServer, SchedulerTick, ServerIdentity}
 import it.unibo.sd1920.akka_raft.utils.NodeRole
 import it.unibo.sd1920.akka_raft.server.LeaderBehaviour
 
@@ -17,12 +19,27 @@ private trait LeaderBehaviour {
 
 
   protected def leaderBehaviour: Receive = clusterBehaviour orElse {
+    case AckAppendEntries =>
     case AppendEntriesResult(res) => handleAppendResult(sender().path.name, res)
+    case req :ClientRequest => handleRequest(req)
+    case RequestVote(candidateTerm, _, lastLogTerm, _) if candidateTerm > currentTerm =>
+    case CommandResult =>
+    case SchedulerTick =>  servers.filter(serverRef=> serverRef._2 != self).foreach(server => server._2 ! AppendEntries(currentTerm, None , None, lastCommittedIndex))
   }
 
   private def handleAppendResult(name: String,value: Boolean): Unit = name match {
     case _ => followersStatusMap = followersStatusMap + (name -> new FollowerStatus(1, 1))
 
+  }
+
+  private def handleRequest(req: ClientRequest): Unit = {
+    val i: Option[Int] = serverLog.getIndexFromReqId(req.requestID)
+    if(serverLog.isReqIdCommitted(req.requestID)){
+      context.children.last ! ApplyCommand(new Entry[BankCommand](req.command, currentTerm, i.get, req.requestID))
+    } else if(i.isEmpty) {
+      var entry = new Entry[BankCommand](req.command, currentTerm, serverLog.size, req.requestID)
+      serverLog.putElementAtIndex(entry)
+    }
   }
 }
 
