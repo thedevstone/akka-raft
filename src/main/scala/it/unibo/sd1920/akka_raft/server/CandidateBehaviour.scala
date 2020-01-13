2b63565fe2b4a660838afef16bbec35adac749b3
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2020-01-13 01:00:57 +0100

    Fixed import problem

--- src/main/scala/it/unibo/sd1920/akka_raft/server/CandidateBehaviour.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/CandidateBehaviour.scala
@@ -14,11 +14,11 @@ private trait CandidateBehaviour {
   protected def candidateBehaviour: Receive = clusterBehaviour orElse {
     case SchedulerTick => restart()
     case req: RequestVoteResult => handleVoteResult(req)
-    case  requestVote: RequestVote => handleRequestVote(requestVote)
+    case requestVote: RequestVote => handleRequestVote(requestVote)
     case _ =>
   }
 
-  private def handleRequestVote(requestVote: RequestVote): Unit =  {
+  private def handleRequestVote(requestVote: RequestVote): Unit = {
     requestVote match {
       case RequestVote(candidateTerm, _, _, _) if candidateTerm <= currentTerm => sender() ! RequestVoteResult(voteGranted = false, currentTerm)
       case RequestVote(candidateTerm, _, lastLogTerm, lastLogIndex) if checkLogBehind(lastLogTerm, lastLogIndex) =>
@@ -33,35 +33,35 @@ private trait CandidateBehaviour {
     lastLogTerm > currentTerm && lastLogIndex > serverLog.lastIndex
   }
 
-  private def handleVoteResult(result: RequestVoteResult): Unit  = result match {
+  private def handleVoteResult(result: RequestVoteResult): Unit = result match {
     case RequestVoteResult(_, followerTerm) if followerTerm > currentTerm => becomingFollower(followerTerm)
     case RequestVoteResult(result, followerTerm) if result && followerTerm == currentTerm => voteCounter += 1
-      if (voteCounter > (NetworkConstants.numberOfServer/2)) becomingLeader()
+      if (voteCounter > (NetworkConstants.numberOfServer / 2)) becomingLeader()
     case _ =>
   }
 
-  private def voteForAnotherCandidate(term: Int){
+  private def voteForAnotherCandidate(term: Int) {
     becomingFollower(term)
     votedFor = Some(sender().path.name)
     sender() ! RequestVoteResult(voteGranted = true, currentTerm)
   }
 
-  private def becomingFollower(term: Int){
+  private def becomingFollower(term: Int) {
     currentTerm = term
     context.become(followerBehaviour)
     startTimer()
   }
 
-  private def becomingLeader(): Unit ={
-    val lastEntry: Option[Entry[BankCommand]] = serverLog.getLastEntry()
+  private def becomingLeader(): Unit = {
+    val lastEntry: Option[Entry[BankCommand]] = serverLog.getLastEntry
     servers.filter(serverRef => serverRef._2 != self).foreach(
-      server => server._2 ! AppendEntries(currentTerm, if (lastEntry.isEmpty) None else serverLog.getPreviousEntry(lastEntry.get) ,lastEntry, lastCommittedIndex))
+      server => server._2 ! AppendEntries(currentTerm, if (lastEntry.isEmpty) None else serverLog.getPreviousEntry(lastEntry.get), lastEntry, lastCommittedIndex))
     context.become(leaderBehaviour)
     startTimer()
   }
 
 
-  private def restart(){
+  private def restart() {
     currentTerm += 1
     voteCounter = 1
     startTimer()
--- src/main/scala/it/unibo/sd1920/akka_raft/server/LeaderBehaviour.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/LeaderBehaviour.scala
@@ -1,33 +1,25 @@
 package it.unibo.sd1920.akka_raft.server
 
-import akka.actor.ActorRef
-import akka.cluster.ClusterEvent.{MemberDowned, MemberUp}
-import akka.cluster.Member
-import akka.routing.ActorRefRoutee
-import it.unibo.sd1920.akka_raft.client.ClientActor
 import it.unibo.sd1920.akka_raft.raft.AppendEntriesResult
-import it.unibo.sd1920.akka_raft.server.ServerActor.{ClientIdentity, IdentifyServer, ServerIdentity}
-import it.unibo.sd1920.akka_raft.utils.NodeRole
-import it.unibo.sd1920.akka_raft.server.LeaderBehaviour
 
 private trait LeaderBehaviour {
   this: ServerActor =>
 
-  private var followersStatusMap: Map[String,FollowerStatus] = Map()
+  private var followersStatusMap: Map[String, FollowerStatus] = Map()
 
 
   protected def leaderBehaviour: Receive = clusterBehaviour orElse {
     case AppendEntriesResult(res) => handleAppendResult(sender().path.name, res)
   }
 
-  private def handleAppendResult(name: String,value: Boolean): Unit = name match {
+  private def handleAppendResult(name: String, value: Boolean): Unit = name match {
     case _ => followersStatusMap = followersStatusMap + (name -> new FollowerStatus(1, 1))
 
   }
 }
 
 
-  class FollowerStatus(nextIndexToSend: Int, lastMatchIndex: Int){
+class FollowerStatus(nextIndexToSend: Int, lastMatchIndex: Int) {
   assert(nextIndexToSend >= 0)
   assert(lastMatchIndex >= 0)
 }
