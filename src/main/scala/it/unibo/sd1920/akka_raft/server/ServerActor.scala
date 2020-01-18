1ca2d04092d14ce6b8e5836806cf43afa68fc952
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2020-01-18 19:47:14 +0100

    Documentation

--- src/main/scala/it/unibo/sd1920/akka_raft/server/ServerActor.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/ServerActor.scala
@@ -18,20 +18,30 @@ import it.unibo.sd1920.akka_raft.utils.ServerRole.ServerRole
 import scala.concurrent.duration._
 import scala.util.Random
 
+/**
+ * Server node of the Akka cluster.
+ */
 private class ServerActor extends Actor with ServerActorDiscovery with LeaderBehaviour with CandidateBehaviour with FollowerBehaviour with ActorLogging with Timers {
+  //CLUSTER NODE
   protected[this] val SERVERS_MAJORITY: Int = (NetworkConstants.numberOfServer / 2) + 1
   protected[this] val cluster: Cluster = Cluster(context.system)
   protected[this] var servers: Map[String, ActorRef] = Map()
   protected[this] var clients: Map[String, ActorRef] = Map()
+  //RAFT STATE
+  protected[this] val serverLog: CommandLog[BankCommand] = CommandLog.emptyLog()
   protected[this] var stateMachineActor: ActorRef = _
+  protected[this] var lastApplied: Int = 0
   protected[this] var currentRole: ServerRole = ServerRole.FOLLOWER
   protected[this] var currentTerm: Int = 0
-  protected[this] var lastApplied: Int = 0
+  protected[this] var votedFor: Option[String] = None
+  //CONTROL STATE
   protected[this] var lastMatched: Int = 0
   protected[this] var stopped: Boolean = false
-  protected[this] var votedFor: Option[String] = None
-  protected[this] val serverLog: CommandLog[BankCommand] = CommandLog.emptyLog()
-  protected[this] var messageLoseThreashold: Double = 1.0
+  protected[this] var messageLossThreshold: Double = 1.0
+
+  /**
+   * Subscribe to cluster and start the state machine.
+   */
   override def preStart(): Unit = {
     cluster.subscribe(self, classOf[MemberUp], classOf[MemberDowned])
     cluster.registerOnMemberUp({
@@ -40,31 +50,36 @@ private class ServerActor extends Actor with ServerActorDiscovery with LeaderBeh
       RaftConstants.maximumStateMachineExecutionTime).millis), "StateMachine")
   }
 
-
+  /**
+   * Intercept all messages and decide if drop some of them. Dropping depends on gui commands.
+   *
+   * @param receiver the receiver
+   */
   case class MessageInterceptor(receiver: Receive) extends Receive {
     def apply(msg: Any): Unit = {
       clients.last._2 ! GuiServerState(ServerVolatileState(currentRole, serverLog.getCommitIndex, lastApplied, votedFor, currentTerm, serverLog.nextIndex, lastMatched, serverLog.getEntries)) //TODO da cancellare
-
       receiver.apply(msg)
       clients.last._2 ! GuiServerState(ServerVolatileState(currentRole, serverLog.getCommitIndex, lastApplied, votedFor, currentTerm, serverLog.nextIndex, lastMatched, serverLog.getEntries)) //TODO da cancellare
     }
     def isDefinedAt(msg: Any): Boolean = {
       clients.last._2 ! GuiServerState(ServerVolatileState(currentRole, serverLog.getCommitIndex, lastApplied, votedFor, currentTerm, serverLog.nextIndex, lastMatched, serverLog.getEntries)) //TODO da cancellare
-
-      if ((stopped || Random.nextDouble() > messageLoseThreashold && (!classOf[InternalMessage].isAssignableFrom(msg.getClass))) && (!classOf[ControlMessage].isAssignableFrom(msg.getClass))) {
+      if ((stopped || Random.nextDouble() > messageLossThreshold && (!classOf[InternalMessage].isAssignableFrom(msg.getClass))) && (!classOf[ControlMessage].isAssignableFrom(msg.getClass))) {
         logWithRole("Messaggio bloccato:: " + msg.toString
         )
         return false
       }
-
       clients.last._2 ! GuiServerState(ServerVolatileState(currentRole, serverLog.getCommitIndex, lastApplied, votedFor, currentTerm, serverLog.nextIndex, lastMatched, serverLog.getEntries)) //TODO da cancellare
-
       receiver.isDefinedAt(msg)
     }
   }
 
   override def receive: Receive = clusterDiscoveryBehaviour
 
+  /**
+   * Behaviour for handling gui control messages.
+   *
+   * @return the behaviour
+   */
   protected def controlBehaviour: Receive = clusterDiscoveryBehaviour orElse {
     case GuiStopServer(_) => stopped = true
       logWithRole("\n\t\tStopped:")
@@ -72,10 +87,15 @@ private class ServerActor extends Actor with ServerActorDiscovery with LeaderBeh
       logWithRole("\n\t\tReasume:")
     case GuiTimeoutServer(_) => handleRequestTimeOut()
     case GuiMsgLossServer(_, loss) => logWithRole("\n\t\tvalore:" + loss)
-      messageLoseThreashold = loss
+      messageLossThreshold = loss
   }
 
-  private def handleRequestTimeOut(){
+  /**
+   * Handle GuiTimeoutServer.
+   * <p>
+   * Send a timeout message that triggers timeout and restart timers.
+   */
+  private def handleRequestTimeOut() {
     self ! SchedulerTick
     currentRole match {
       case ServerRole.FOLLOWER => startTimeoutTimer()
@@ -84,18 +104,34 @@ private class ServerActor extends Actor with ServerActorDiscovery with LeaderBeh
     }
   }
 
+  /**
+   * Start the timeout timer.
+   */
   protected def startTimeoutTimer(): Unit = {
     timers startTimerWithFixedDelay(SchedulerTickKey, SchedulerTick, RandomUtil.randomBetween(RaftConstants.minimumTimeout, RaftConstants.maximumTimeout) millis)
   }
 
+  /**
+   * Start the heartbeat timer.
+   */
   protected def startHeartbeatTimer(): Unit = {
     timers startTimerWithFixedDelay(SchedulerTickKey, SchedulerTick, RaftConstants.heartbeatTimeout millis)
   }
 
+  /**
+   * Broadcast a raft message to all servers.
+   *
+   * @param raftMessage the message
+   */
   protected def broadcastMessage(raftMessage: RaftMessage): Unit = {
     servers.filter(s => s._2 != self).foreach(v => v._2 ! raftMessage)
   }
 
+  /**
+   * Commit all the entries up to index. Then apply all committed entries to state machine.
+   *
+   * @param index the commit index
+   */
   protected def callCommit(index: Int): Unit = {
     val lastCommitted: Int = serverLog.getCommitIndex
     serverLog.commit(index)
@@ -103,10 +139,22 @@ private class ServerActor extends Actor with ServerActorDiscovery with LeaderBeh
     lastApplied = index
   }
 
+  /**
+   * Check if a server can vote another server.
+   *
+   * @param lastLogTerm  the last log term
+   * @param lastLogIndex the last log index
+   * @return true if can vote, false if can't vote
+   */
   protected def checkElectionRestriction(lastLogTerm: Int, lastLogIndex: Int): Boolean = {
     lastLogTerm >= serverLog.lastTerm && lastLogIndex >= serverLog.lastIndex
   }
 
+  /**
+   * Log message with the server role.
+   *
+   * @param msg the message
+   */
   protected def logWithRole(msg: String): Unit = log info s"${self.path.name}:$currentRole -> $msg"
 }
 
--- src/main/scala/it/unibo/sd1920/akka_raft/server/ServerActorDiscovery.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/ServerActorDiscovery.scala
@@ -6,6 +6,9 @@ import it.unibo.sd1920.akka_raft.client.ClientActor
 import it.unibo.sd1920.akka_raft.server.ServerActor.{ClientIdentity, IdentifyServer, ServerIdentity}
 import it.unibo.sd1920.akka_raft.utils.NodeRole
 
+/**
+ * Node discovery behaviour in Akka Cluster.
+ */
 private trait ServerActorDiscovery {
   this: ServerActor =>
 
@@ -18,7 +21,11 @@ private trait ServerActorDiscovery {
     case ServerIdentity(name: String) => addServer(name)
   }
 
-
+  /**
+   * Manage a new member event.
+   *
+   * @param member the member that joined the cluster and become up
+   */
   private def manageNewMember(member: Member): Unit = member match {
     case m if member.roles.contains("server") =>
       context.system.actorSelection(s"${m.address}/user/**") ! ServerActor.IdentifyServer(NodeRole.SERVER)
@@ -27,10 +34,24 @@ private trait ServerActorDiscovery {
     case _ =>
   }
 
+  /**
+   * Handle ClientIdentity
+   *
+   * Add client to clients map when client is identified
+   *
+   * @param name client id
+   */
   private def addClient(name: String): Unit = {
     this.clients = this.clients + (name -> sender())
   }
 
+  /**
+   * Handle ServerIdentity
+   *
+   * Add server to servers map when server is identified
+   *
+   * @param name server id
+   */
   private def addServer(name: String): Unit = {
     this.servers = this.servers + (name -> sender())
     if (servers.size >= 5) {
