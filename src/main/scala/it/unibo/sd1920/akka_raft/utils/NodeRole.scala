fc6861c2680c3dafdb1aa7145149c936a94ea7aa
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2020-01-18 16:03:29 +0100

    Utils documentation

--- src/main/scala/it/unibo/sd1920/akka_raft/utils/NodeRole.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/utils/NodeRole.scala
@@ -1,5 +1,8 @@
 package it.unibo.sd1920.akka_raft.utils
 
+/**
+ * Server node role used in node discovery in Akka Clustering.
+ */
 object NodeRole {
   sealed abstract class NodeRole()
   case object SERVER extends NodeRole
