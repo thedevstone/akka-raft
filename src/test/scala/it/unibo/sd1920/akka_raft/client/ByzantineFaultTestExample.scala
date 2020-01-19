bd38e245c5180f40f45d57d3115b806ab01af8ea
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2020-01-19 23:28:39 +0100

    Fixed test ignoring

--- src/test/scala/it/unibo/sd1920/akka_raft/client/ByzantineFaultTestExample.scala
+++ src/test/scala/it/unibo/sd1920/akka_raft/client/ByzantineFaultTestExample.scala
@@ -14,6 +14,7 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.wordspec.AnyWordSpecLike
 
 import scala.util.Random
+
 //Questo test visivo molto grezzo, concepito per mettere in luce in maniera evidente la non resistenza di RAFT a bizzantine failure.
 //Il test inizializza un cluster di server che riceve messaggi da propagare nel tempo, per ogni messaggio inviato al cluster,
 //Un server casuale genera un messaggio random che viene spedito ad un mittente random, questo porta a comportamenti anomali,
@@ -61,7 +62,7 @@ class ByzantineFaultTestExample
 
   private def getConfig(config: String): com.typesafe.config.Config = {
     var port = config
-    if(port.isEmpty) port = "0"
+    if (port.isEmpty) port = "0"
     ConfigFactory.parseString(s"""akka.remote.artery.canonical.port=$port""")
       .withFallback(ConfigFactory.load("server"))
   }
@@ -76,12 +77,12 @@ class ByzantineFaultTestExample
   }
   private def getRandomCommand: BankCommand = {
 
-    def value:Int = Random.nextInt(30)
-    if(value >= 20) {
+    def value: Int = Random.nextInt(30)
+    if (value >= 20) {
       Withdraw(Random.nextInt(1000).toString, Random.nextInt(1000))
-    }else if (value >= 10){
+    } else if (value >= 10) {
       Deposit(Random.nextInt(1000).toString, Random.nextInt(1000))
-    }else{
+    } else {
       GetBalance(Random.nextInt(1000).toString)
     }
 
@@ -89,36 +90,36 @@ class ByzantineFaultTestExample
   }
 
   private def getRandomEntry: Option[Entry[BankCommand]] = {
-    if(Random.nextBoolean()){
+    if (Random.nextBoolean()) {
       Some(Entry[BankCommand](getRandomCommand, Random.nextInt(1000), Random.nextInt(1000), Random.nextInt(1000)))
-    }else{
+    } else {
       None
     }
   }
 
   private def getRandomMessage(actorRef: ActorRef): Any = {
-    if(Random.nextBoolean()){
+    if (Random.nextBoolean()) {
       RequestVote(Random.nextInt(1000), actorRef, Random.nextInt(1000), Random.nextInt(1000))
-    }else{
-      AppendEntries(Random.nextInt(1000), getRandomEntry, getRandomEntry ,Random.nextInt(1000))
+    } else {
+      AppendEntries(Random.nextInt(1000), getRandomEntry, getRandomEntry, Random.nextInt(1000))
     }
   }
 
   private def randomBehavior(actorRef: ActorRef, name: String): Any = {
 
-    Stream.range(0,Random.nextInt(6)).foreach(_ => {
-      if(Random.nextBoolean()) clientGuiActor.tell(GuiTimeoutServer(name),clientGuiActor)
+    Stream.range(0, Random.nextInt(6)).foreach(_ => {
+      if (Random.nextBoolean()) clientGuiActor.tell(GuiTimeoutServer(name), clientGuiActor)
       val byzantineTarget: ActorRef = getRandomServerRef
-      byzantineTarget.tell(getRandomMessage(actorRef),actorRef)
+      byzantineTarget.tell(getRandomMessage(actorRef), actorRef)
     })
   }
 
   //Inizializzo il cluster generando 5 e un solo client
   override def beforeAll(): Unit = {
-    serversName = List("S0","S1","S2","S3","S4")
+    serversName = List("S0", "S1", "S2", "S3", "S4")
     serversRef = List()
 
-     clientSystem0 = ActorSystem(NetworkConstants.clusterName, ConfigFactory.parseString("""akka.remote.artery.canonical.port=5000""")
+    clientSystem0 = ActorSystem(NetworkConstants.clusterName, ConfigFactory.parseString("""akka.remote.artery.canonical.port=5000""")
       .withFallback(ConfigFactory.load("client")))
     clientGuiActor = clientSystem0 actorOf(ClientActor.props, "C0")
     serverSystem0 = ActorSystem(NetworkConstants.clusterName, getConfig(NetworkConstants.secondSeedPort.toString))
@@ -140,15 +141,15 @@ class ByzantineFaultTestExample
   }
 
   "Testing Cluster long running" must {
-    "" in {
+    "" ignore {
       //Attendo che tutto sia online
       Thread.sleep(20000)
       //S0 è considerato il server byzzantino
       //Sottometto al cluster una serie di richieste, seguite da una serie di messaggi "bizzantini", messaggi generati e spediti con
       //L'indirizzo di in un server casuale che portano scompiglio, l'effetto più immediato è l'assenza nel server di un Leader fisso.
       //Possono presentarsi anche comportamenti non conteplati.
-      Stream.range(0,40).foreach(_ => {
-        clientGuiActor.tell(GuiSendMessage(getRandomServerName , CommandType(Random.nextInt(3)), Random.nextInt(100).toString , Random.nextInt(100).toString), clientGuiActor)
+      Stream.range(0, 40).foreach(_ => {
+        clientGuiActor.tell(GuiSendMessage(getRandomServerName, CommandType(Random.nextInt(3)), Random.nextInt(100).toString, Random.nextInt(100).toString), clientGuiActor)
         randomBehavior(serverActor0, serverActor0.path.name)
         Thread.sleep(500)
       })
--- src/test/scala/it/unibo/sd1920/akka_raft/client/LongRunningTest.scala
+++ src/test/scala/it/unibo/sd1920/akka_raft/client/LongRunningTest.scala
@@ -11,6 +11,7 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.wordspec.AnyWordSpecLike
 
 import scala.util.Random
+
 //Un test visivo che automatizza l'invio di alcuni messaggi(con e senza perdita), per osservare se il comportamento del cluster è corretto.
 //
 class LongRunningTest
@@ -54,7 +55,7 @@ class LongRunningTest
 
   private def getConfig(config: String): com.typesafe.config.Config = {
     var port = config
-    if(port.isEmpty) port = "0"
+    if (port.isEmpty) port = "0"
     ConfigFactory.parseString(s"""akka.remote.artery.canonical.port=$port""")
       .withFallback(ConfigFactory.load("server"))
   }
@@ -65,45 +66,45 @@ class LongRunningTest
 
   //Inizializzo il cluster
   override def beforeAll(): Unit = {
-    serversName = List("S0","S1","S2","S3","S4")
-     clientSystem0 = ActorSystem(NetworkConstants.clusterName, ConfigFactory.parseString("""akka.remote.artery.canonical.port=5000""")
+    serversName = List("S0", "S1", "S2", "S3", "S4")
+    clientSystem0 = ActorSystem(NetworkConstants.clusterName, ConfigFactory.parseString("""akka.remote.artery.canonical.port=5000""")
       .withFallback(ConfigFactory.load("client")))
     clientGuiActor = clientSystem0 actorOf(ClientActor.props, "C0")
-     serverSystem0 = ActorSystem(NetworkConstants.clusterName, getConfig(NetworkConstants.secondSeedPort.toString))
+    serverSystem0 = ActorSystem(NetworkConstants.clusterName, getConfig(NetworkConstants.secondSeedPort.toString))
     serverActor0 = serverSystem0 actorOf(ServerActor.props, "S0")
 
 
-     serverSystem1 = ActorSystem(NetworkConstants.clusterName, getConfig(""))
+    serverSystem1 = ActorSystem(NetworkConstants.clusterName, getConfig(""))
     serverActor1 = serverSystem1 actorOf(ServerActor.props, "S1")
-     serverSystem2 = ActorSystem(NetworkConstants.clusterName, getConfig(""))
+    serverSystem2 = ActorSystem(NetworkConstants.clusterName, getConfig(""))
     serverActor2 = serverSystem2 actorOf(ServerActor.props, "S2")
-     serverSystem3 = ActorSystem(NetworkConstants.clusterName, getConfig(""))
+    serverSystem3 = ActorSystem(NetworkConstants.clusterName, getConfig(""))
     serverActor3 = serverSystem3 actorOf(ServerActor.props, "S3")
-     serverSystem4 = ActorSystem(NetworkConstants.clusterName, getConfig(""))
+    serverSystem4 = ActorSystem(NetworkConstants.clusterName, getConfig(""))
     serverActor4 = serverSystem4 actorOf(ServerActor.props, "S4")
   }
 
   "Testing Cluster long running" must {
-    "" in {
+    "" ignore {
       //Attendo che tutto sia online
       Thread.sleep(20000)
       //Sottometto al cluster una serie di richieste, senza perdità di messaggi
 
-      Stream.range(0,15).foreach(_ => {
-        clientGuiActor.tell(GuiSendMessage(getRandomServer , CommandType(Random.nextInt(3)), Random.nextInt(100).toString , Random.nextInt(100).toString), clientGuiActor)
+      Stream.range(0, 15).foreach(_ => {
+        clientGuiActor.tell(GuiSendMessage(getRandomServer, CommandType(Random.nextInt(3)), Random.nextInt(100).toString, Random.nextInt(100).toString), clientGuiActor)
         Thread.sleep(500)
       })
       //Imposto la perdità di messaggi per ogni server al 25%
-      clientGuiActor.tell(GuiMsgLossServer("S0",0.75), clientGuiActor)
-      clientGuiActor.tell(GuiMsgLossServer("S1",0.75), clientGuiActor)
-      clientGuiActor.tell(GuiMsgLossServer("S2",0.75), clientGuiActor)
-      clientGuiActor.tell(GuiMsgLossServer("S3",0.75), clientGuiActor)
-      clientGuiActor.tell(GuiMsgLossServer("S4",0.75), clientGuiActor)
+      clientGuiActor.tell(GuiMsgLossServer("S0", 0.75), clientGuiActor)
+      clientGuiActor.tell(GuiMsgLossServer("S1", 0.75), clientGuiActor)
+      clientGuiActor.tell(GuiMsgLossServer("S2", 0.75), clientGuiActor)
+      clientGuiActor.tell(GuiMsgLossServer("S3", 0.75), clientGuiActor)
+      clientGuiActor.tell(GuiMsgLossServer("S4", 0.75), clientGuiActor)
       Thread.sleep(5000)
 
       //Sottometto al cluster una serie di richieste, con una perdità del 25%
-      Stream.range(0,25).foreach(_ => {
-        clientGuiActor.tell(GuiSendMessage(getRandomServer , CommandType(Random.nextInt(3)), Random.nextInt(100).toString , Random.nextInt(100).toString), clientGuiActor)
+      Stream.range(0, 25).foreach(_ => {
+        clientGuiActor.tell(GuiSendMessage(getRandomServer, CommandType(Random.nextInt(3)), Random.nextInt(100).toString, Random.nextInt(100).toString), clientGuiActor)
         Thread.sleep(500)
       })
 
