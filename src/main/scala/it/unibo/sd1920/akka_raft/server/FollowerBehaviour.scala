9d3e44b7af6d2ea8a180ca9763206b7d13643a4b
Luca Giulianini <luca.giulianini2@studio.unibo.it>
2020-01-19 20:36:12 +0100

    Refactoring follower

--- src/main/scala/it/unibo/sd1920/akka_raft/server/FollowerBehaviour.scala
+++ src/main/scala/it/unibo/sd1920/akka_raft/server/FollowerBehaviour.scala
@@ -61,41 +61,41 @@ private trait FollowerBehaviour {
    */
   private def handleAppendEntries(appendEntry: AppendEntries): Unit = {
     leaderRef = Some(sender())
+    var success: Boolean = false
+    var lastMatchedIndex: Int = -1
     checkAndUpdateTerm(appendEntry.leaderTerm)
     appendEntry match {
-      // ############# SPECIAL CASES
       //reject message from leader behind me
       case AppendEntries(leaderTerm, _, _, _) if leaderTerm < currentTerm =>
-        lastMatched = -1
-        sender() ! AppendEntriesResult(success = false, lastMatched, currentTerm)
+        lastMatchedIndex = -1
+        success = false
       //leader has empty log and send empty previous entry and empty entry. So empty heartbeat
       case AppendEntries(_, previousEntry, entry, _) if previousEntry.isEmpty && entry.isEmpty =>
-        lastMatched = -1
-        sender() ! AppendEntriesResult(success = true, lastMatched, currentTerm)
+        lastMatchedIndex = -1
+        success = true
       //leader send first entry presents in log, previous is empty. Insert entry and handle commit
       case AppendEntries(_, previousEntry, entry, leaderLastCommit) if previousEntry.isEmpty && entry.nonEmpty =>
-        val result: Boolean = serverLog.insertEntry(entry.get)
+        success = serverLog.insertEntry(entry.get)
         handleCommit(leaderLastCommit)
-        lastMatched = 0
-        sender() ! AppendEntriesResult(success = result, lastMatched, currentTerm)
-      // ############# NORMAL OPERATIONS
+        lastMatchedIndex = 0
       //consistency check totally fails. If follower does not contains previous entry then all log diverges
       case AppendEntries(_, previousEntry, _, _) if !serverLog.contains(previousEntry.get) =>
-        lastMatched = -1
-        sender() ! AppendEntriesResult(success = false, lastMatched, currentTerm)
+        lastMatchedIndex = -1
+        success = false
       //consistency check works and previous entry is present. insert entry and handle commit.
       case AppendEntries(_, _, entry, leaderLastCommit) if entry.nonEmpty =>
-        val result: Boolean = serverLog.insertEntry(entry.get)
+        success = serverLog.insertEntry(entry.get)
         handleCommit(leaderLastCommit)
-        lastMatched = entry.get.index
-        sender() ! AppendEntriesResult(success = result, lastMatched, currentTerm)
+        lastMatchedIndex = entry.get.index
       //if simple heartbeat return true and perform commit
       case AppendEntries(_, previousEntry, _, leaderLastCommit) =>
         handleCommit(leaderLastCommit)
-        lastMatched = previousEntry.get.index
-        sender() ! AppendEntriesResult(success = true, lastMatched, currentTerm)
+        lastMatchedIndex = previousEntry.get.index
+        success = true
       case _ =>
     }
+    lastMatched = lastMatchedIndex
+    sender() ! AppendEntriesResult(success, lastMatchedIndex, currentTerm)
     startTimeoutTimer()
   }
 
